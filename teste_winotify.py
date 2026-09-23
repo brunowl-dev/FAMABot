cff129d4d7464b61f3b9875f6a0cb011afce2fa1
Bruno Wai Lu <brunoantoniowl@gmail.com>
2026-09-23 17:32:41 -0300

    add: winotify com logo e mais setores como opção

--- teste_winotify.py
+++
@@ -1,9 +0,0 @@
-from winotify import Notification
-
-toast = Notification(
-    app_id="FAMA Notificações",
-    title="Novo atendimento",
-    msg="Cliente selecionou o setor Fiscal",
-    icon=r"c:/users/visitante/Documents/FAMABot/logo.png"
-)
-toast.show()
